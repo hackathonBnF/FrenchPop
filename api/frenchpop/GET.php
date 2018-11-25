56c17000bb22b3390e8cb04b6f168b80d09c4020
Didier BELLAMY <dbellamy@sigb.net>
2018-11-25 05:17:58 +0100

    page thematique

--- api/frenchpop/GET.php
+++ api/frenchpop/GET.php
@@ -6,10 +6,9 @@ use frenchpop\FrenchPop;
 
 class GET extends Controler {
     
-   public function accueil() {
-        //theme alea
-	$r = FrenchPop::query('select count(*) from thematiques');
-	$n = $r->num_rows;
+  public function accueil() {
+	$r = FrenchPop::query('select count(*) as n from thematiques');
+	$n = $r->fetch_object()->n;
 	$x = rand(0,$n-1);
 	$r = FrenchPop::query("select id_thematique, label from thematiques limit $x,1");
 	$thematique = $r->fetch_object();
@@ -20,5 +19,18 @@ class GET extends Controler {
 	return FrenchPop::render(__DIR__."/resources/accueil.html", $data);
 
     }
-}
+    
+    
+    public function thematiques() {
+      $id_thematique=$this->getQV('id_thematique');
+      $r = FrenchPop::query("select label from thematiques where id_thematique=".$id_thematique);
+      $label = $r->fetch_object()->label;
+      $data = [
+	  'id'=>$id_thematique, 
+	  'label'=>$label
+      ];
+      return FrenchPop::render(__DIR__."/resources/thematiques.html", $data);
 
+    }
+    
+}
