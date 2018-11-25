c2cc947b4b15d1e6457848c58d0ecd2ef87d8828
arenou <arenou@sigb.net>
2018-11-25 02:40:06 +0100

    dbpedia

---
+++ app/endpoint/dbpedia.php
@@ -0,0 +1,11 @@
+<?php
+namespace frenchpop\app\endpoint;
+
+use frenchpop\Frenchpop;
+
+class dbpedia extends endpoint {
+    protected $name = 'dbpedia';
+    protected $url = 'https://dbpedia.org/sparql';
+
+    
+}
